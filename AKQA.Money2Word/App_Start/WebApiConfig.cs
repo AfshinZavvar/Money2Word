442b6512cc1a7f7526c6515363ea47c3a7c0cac4
Afshin <afshin.zavar@gmail.com>
2021-10-31 20:46:19 +1100

    enabled cors origin

--- AKQA.Money2Word/App_Start/WebApiConfig.cs
+++ AKQA.Money2Word/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Money2Word
 {
@@ -10,15 +11,22 @@ namespace Money2Word
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            string origins = GetAllowedOrigins();
+            var cors = new EnableCorsAttribute(origins, "*", "post");
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+        }
+        private static string GetAllowedOrigins()
+        {
+            //Make a call to the database to get allowed origins and convert to a comma separated string
+            return "http://money2word.azurewebsites.net,https://money2word.azurewebsites.net,http://localhost:18075";
         }
     }
 }
